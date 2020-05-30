f6ad4cb8d07e34b0674002fc2d8fc619082f8674
Hafiz Zain <onlyyzain@gmail.com>
2020-05-30 14:17:10 +0400

    django project

---
+++ ecommrce_project/store/urls.py
@@ -0,0 +1,24 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('home/', views.home, name='home'),
+    path('category/<slug:category_slug>', views.home, name='products_by_category'),
+    path('category/<slug:category_slug>/<slug:product_slug>', views.productPage, name='product_detail'),
+    path('cart/add/<int:product_id>', views.add_cart, name='add_cart'),
+    path('cart/remove/<int:product_id>', views.cart_remove, name='cart_remove'),
+    path('cart/delete/<int:product_id>', views.cart_delete, name='cart_delete'),
+    path('cart', views.cart_detail, name='cart_detail'),
+    path('thankyou/<int:order_id>', views.thanks_page, name='thanks_page'),
+    path('account/create/', views.signupView, name='signup'),
+    path('account/signin/', views.signinView, name='signin'),
+    path('account/signout/', views.signoutView, name='signout'),
+    path('order_history/', views.orderHistory, name='order_history'),
+    path('order/<int:order_id>', views.viewOrder, name='order_detail'),
+    path('search/', views.search, name='search'),
+
+
+
+
+
+]
